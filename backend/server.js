3bfe1a7406082c5d52fcb5f3cace5eb09a78dbe0
DuongNguyenCoder <nguyenvanduong240104@gmail.com>
2026-04-17 21:18:44 +0700

    update cors

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,28 @@
-const express = require('express')
-require('dotenv').config()
-const cors = require('cors')
-const initRoutes = require('./routes')
-const dbConnected = require('./configs/dbConnected')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const initRoutes = require("./routes");
+const dbConnected = require("./configs/dbConnected");
+const cookieParser = require("cookie-parser");
 
-const app = express()
-const port = process.env.PORT || 8888
+const app = express();
+const port = process.env.PORT || 8888;
 
-app.use(express.json({ limit: '100mb' }))
-app.use(express.urlencoded({ limit: '100mb', extended: true }))
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 // CORS configuration
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'https://minhdental.com',
-      'https://duoclieuphattam.com',
-      process.env.CLIENT_URL,
-    ],
+    origin: "https://minhdental.com",
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
-)
-app.use(cookieParser())
+);
+app.use(cookieParser());
 
-initRoutes(app)
-dbConnected()
+initRoutes(app);
+dbConnected();
 
-app.listen(port, () => console.log(`server on port: ${port}`))
+app.listen(port, () => console.log(`server on port: ${port}`));
